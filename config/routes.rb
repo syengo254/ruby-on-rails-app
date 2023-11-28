b38054d4ded37c12568410cd2a32d27ae862043e
David Syengo <d.syengo@institute.global>
2023-11-28 22:20:19 +0300

    wip: added user create route and form

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "sign-up" => "registrations#new", as: :sign_up
+
+  post "sign-up" => "registrations#create"
+
   get "about", to: "about#index", as: :about
 
   get "contact-us", to: "main#contact", as: :contact
