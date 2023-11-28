e130fad61195cded02d5f34da787d917e317bde0
David Syengo <d.syengo@institute.global>
2023-11-28 20:54:53 +0300

    feat: flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,7 @@
 class MainController < ApplicationController
   def index
-
+    flash.now[:notice] = "logged in succefully!"
+    flash.now[:alert] = "Invalid email or password!"
   end
 
   def contact
